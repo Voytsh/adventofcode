21a9fe1899381e1757809bd7383bebab491e1fdb
Voytsh <wojak08@icloud.com>
2022-12-03 12:32:35 +0100

    d1

--- D1/z1.py
+++ D1/z1.py
@@ -1,16 +1,23 @@
-if __name__ == '__main__':
+def get_list_of_calories():
     tmp = 0
-    one_elf_total_calories = []
-    with open('d1z1.txt') as f:
+    result = []
+    with open('D1/d1z1.txt') as f:
         for line in f:
             if line != '\n':
                 tmp += int(line)
             else:
-                one_elf_total_calories.append(tmp)
+                result.append(tmp)
                 tmp = 0
+    return result
 
-        bq = max(one_elf_total_calories)
-        print(f'The most calories in total of {bq} is carried by the elf #{one_elf_total_calories.index(bq)}')
+if __name__ == '__main__':
+    one_elf_total_calories = get_list_of_calories()
+    bq = max(one_elf_total_calories)
 
+    print("z1")
+    print(f'The most calories in total of {bq} is carried by the elf #{one_elf_total_calories.index(bq)}')
+
+    print("\nz2")
+    print(sum(sorted(one_elf_total_calories, reverse=True)[:3]))
 
 
