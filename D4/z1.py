c59169f432e0e24c37141a58907edcea8ddda9ef
Voytsh <wojak08@icloud.com>
2022-12-04 13:05:02 +0100

    D4 z1

---
+++ D4/z1.py
@@ -0,0 +1,34 @@
+def get_section_assignment():
+    lista = []
+    with open('/Users/filipwojcieszak/Documents/Adventofcode/D4/d4.txt', 'r') as f:
+        for line in f:
+            pair = line.split(",")
+            tmp = [pair[0], pair[1].strip()]
+            lista.append(tmp)
+    return lista
+
+
+def get_pairs_that_fully_contain_other(lista):
+    idxs = []
+    for index, item in enumerate(lista, start=1):
+        first_elf = item[0]
+        second_elf = item[1]
+        first_elf = first_elf.split("-")
+        second_elf = second_elf.split("-")
+        first_elf_int = list(map(int, first_elf))
+        second_elf_int = list(map(int, second_elf))
+
+        if (first_elf_int[0] <= second_elf_int[0]) and (first_elf_int[1] >= second_elf_int[1]) or (
+                (first_elf_int[0] >= second_elf_int[0]) and (first_elf_int[1] <= second_elf_int[1])):
+            idxs.append(index)
+            print(index, first_elf, second_elf)
+
+    return idxs
+
+
+if __name__ == '__main__':
+    lista = get_section_assignment()
+    x = get_pairs_that_fully_contain_other(lista)
+    print(
+        f"In how many assignment pairs does one range fully contain the other? Answer: {len(x)}")
+    print(x)
