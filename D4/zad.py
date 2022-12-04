e1f1812bb8cbba90b6ffbe759e6a19dd372f0e21
Voytsh <wojak08@icloud.com>
2022-12-04 14:01:08 +0100

    Na pewno ;))?

--- D4/zad.py
+++ D4/zad.py
@@ -16,19 +16,14 @@ def get_answer(lista):
         first_elf_int = list(map(int, first_elf.split("-")))
         second_elf_int = list(map(int, second_elf.split("-")))
 
-        if (second_elf_int[0] <= first_elf_int[0] <= second_elf_int[1]) or (
-                second_elf_int[0] <= first_elf_int[1] <= second_elf_int[1]) or (
-                first_elf_int[0] <= second_elf_int[0] <= first_elf_int[1]) or (
-                first_elf_int[0] <= second_elf_int[1] <= first_elf_int[1]):
+        if (second_elf_int[0] <= first_elf_int[0] <= second_elf_int[1]) or (second_elf_int[0] <= first_elf_int[1] <= second_elf_int[1]) or (first_elf_int[0] <= second_elf_int[0] <= first_elf_int[1]) or (first_elf_int[0] <= second_elf_int[1] <= first_elf_int[1]):
             idxs_part2.append(index)
 
-        if (first_elf_int[0] <= second_elf_int[0]) and (first_elf_int[1] >= second_elf_int[1]) or (
-                (first_elf_int[0] >= second_elf_int[0]) and (first_elf_int[1] <= second_elf_int[1])):
+        if (first_elf_int[0] <= second_elf_int[0]) and (first_elf_int[1] >= second_elf_int[1]) or ((first_elf_int[0] >= second_elf_int[0]) and (first_elf_int[1] <= second_elf_int[1])):
             idxs_part1.append(index)
     return idxs_part1, idxs_part2
 
 
 if __name__ == '__main__':
-    lista = get_section_assignment()
-    part1_answer, part2_answer = get_answer(lista)
+    part1_answer, part2_answer = get_answer(get_section_assignment())
     print(f"Answer: {len(part1_answer), len(part2_answer)}")
