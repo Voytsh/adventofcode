0fd5b0d7d5556a59ba7c29fd3918d4b0e38759d7
Voytsh <wojak08@icloud.com>
2022-12-05 12:11:06 +0100

    D4 CLEANUP

--- D4/zad.py
+++ D4/zad.py
@@ -14,10 +14,14 @@ def get_answer(lista):
         first_elf_int = list(map(int, item[0].split("-")))
         second_elf_int = list(map(int, item[1].split("-")))
 
-        if (second_elf_int[0] <= first_elf_int[0] <= second_elf_int[1]) or (second_elf_int[0] <= first_elf_int[1] <= second_elf_int[1]) or (first_elf_int[0] <= second_elf_int[0] <= first_elf_int[1]) or (first_elf_int[0] <= second_elf_int[1] <= first_elf_int[1]):
+        if (second_elf_int[0] <= first_elf_int[0] <= second_elf_int[1]) or (
+                second_elf_int[0] <= first_elf_int[1] <= second_elf_int[1]) or (
+                first_elf_int[0] <= second_elf_int[0] <= first_elf_int[1]) or (
+                first_elf_int[0] <= second_elf_int[1] <= first_elf_int[1]):
             idxs_part2.append(index)
 
-        if (first_elf_int[0] <= second_elf_int[0]) and (first_elf_int[1] >= second_elf_int[1]) or ((first_elf_int[0] >= second_elf_int[0]) and (first_elf_int[1] <= second_elf_int[1])):
+        if (first_elf_int[0] <= second_elf_int[0]) and (first_elf_int[1] >= second_elf_int[1]) or (
+                (first_elf_int[0] >= second_elf_int[0]) and (first_elf_int[1] <= second_elf_int[1])):
             idxs_part1.append(index)
     return idxs_part1, idxs_part2
 
