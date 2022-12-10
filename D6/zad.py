25f4bdb19a2cbb39eeead543d4c695ad21832637
Voytsh <wojak08@icloud.com>
2022-12-10 13:45:47 +0100

    D6 Done

---
+++ D6/zad.py
@@ -0,0 +1,14 @@
+def zad(N):
+    with open('/Users/filipwojcieszak/Documents/Adventofcode/D6/input.txt', 'r') as f:
+        file_contents = f.read()
+        for i in range(len(file_contents)-(N-1)):
+            tmp = set(file_contents[i: i+N])
+            if len(tmp) == N:
+                print(i+N)
+                return
+
+
+
+if __name__ == '__main__':
+    zad(4)
+    zad(14)
